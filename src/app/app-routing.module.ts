fb5eae15f399bb9bb3c2aea51abc78a976222eb1
Ana Vázquez <ana.maria.vl@hotmail.com>
2019-01-31 23:20:28 -0600

    Primeros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Navbar2Component } from './components/navbar2/navbar2.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { AlarmsComponent } from './components/alarms/alarms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'navbar2', component: Navbar2Component},
+  {path: 'carousel', component: CarouselComponent},
+  {path: 'alarmas', component: AlarmsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { Navbar2Component } from './components/navbar2/navbar2.component';
+import { AlarmsComponent } from './components/alarms/alarms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CarouselComponent,
+    Navbar2Component,
+    AlarmsComponent
   ],
   imports: [
     BrowserModule,
